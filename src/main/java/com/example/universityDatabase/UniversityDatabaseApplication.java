f9ebff992891c69d18abe30243abd8525c4b1934
Maxim Gura <maxim.gura@plus4u.net>
2023-10-05 21:00:09 +0200

    First commit Hello world!

---
+++ src/main/java/com/example/universityDatabase/UniversityDatabaseApplication.java
@@ -0,0 +1,15 @@
+package com.example.universityDatabase;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UniversityDatabaseApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(UniversityDatabaseApplication.class, args);
+
+    System.out.println("Hello world!");
+  }
+
+}
